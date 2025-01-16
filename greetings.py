76be590a5fec1b768fcb21a20282ba597fc6d35b
duxles10 <68594761+duxles10@users.noreply.github.com>
2025-01-16 20:05:23 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,23 @@
+username = 'duxles'
+
+title = 'План на новый год'
+
+content = 'Завести кошку или собаку'
+
+status = 'Открыт'
+
+created_date = '1-1-2025'
+
+issue_date = '31-12-2025'
+
+print('Имя пользователя:', username)
+
+print('Заголовок заметки:', title)
+
+print('Описание заметки:', content)
+
+print('Статус заметки:', status)
+
+print('Дата создания заметки:', created_date)
+
+print('Дата истечения заметки:', issue_date)
