87002dd3f574a2c46c556470e2e2c77af70b7ee5
duxles10 <68594761+duxles10@users.noreply.github.com>
2025-01-17 21:41:32 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,23 @@
+username = input('Введите имя: ')
+
+title = input('Введите заголовок: ')
+
+content = input('Введите описание: ')
+
+status = input('Укажите статус заметки: ')
+
+created_date = input('Дата создания дд-мм-гггг: ')
+
+issue_date = input('Дата окончания дд-мм-гггг: ')
+
+print('Имя пользователя:', username)
+
+print('Заголовок заметки:', title)
+
+print('Описание заметки:', content)
+
+print('Статус заметки:', status)
+
+print('Дата создания заметки:', created_date)
+
+print('Дата истечения заметки:', issue_date)
